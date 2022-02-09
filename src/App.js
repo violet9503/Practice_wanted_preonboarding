ab1fdf94a71d798fc384c9bc4aa5a2d8c7b815c5
violet9503 <violet9503@gmail.com>
2022-02-09 23:23:22 +0900

    Toggle 컴포넌트 중간 과정 커밋

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Toggle from "./component/Toggle";
+import Animate from "./Animate";
 
 function App() {
   return (
     <div className="App">
       <Toggle />
+      {/* <Animate /> */}
     </div>
   );
 }
--- src/component/Toggle.js
+++ src/component/Toggle.js
@@ -1,22 +1,47 @@
 import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 const ToggleBlock = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-start;
-  width: 70px;
-  padding: 5px;
+  width: 80px;
   border-radius: 20px;
   background-color: #bbb;
   cursor: pointer;
   margin-top: 20px;
   margin-bottom: 10px;
+
+  ::before {
+    content: "";
+    display: flex;
+    width: 0px;
+    height: 2em;
+    border-radius: 20px;
+    background-color: #6741d9;
+    position: absolute;
+  }
+
+  ::after {
+    content: "";
+    display: flex;
+    border-radius: 1.5em;
+    width: 1.5em;
+    height: 1.5em;
+    margin: 5px;
+    background-color: white;
+    transition: transform 0.3s ease-in-out;
+  }
+
   ${({ toggle }) =>
     toggle &&
     css`
-      justify-content: flex-end;
-      background-color: #6741d9;
+      ::before {
+        width: 80px;
+        transition: width 0.3s ease-in;
+      }
+      ::after {
+        transform: translateX(45px);
+      }
     `}
 `;
 
@@ -38,7 +63,7 @@ function Toggle() {
   return (
     <>
       <ToggleBlock onClick={onToggle} toggle={toggle}>
-        <ToggleCircle></ToggleCircle>
+        {/* <ToggleCircle></ToggleCircle> */}
       </ToggleBlock>
       <div>Toggle Switch {toggle ? "ON" : "OFF"}</div>
     </>
